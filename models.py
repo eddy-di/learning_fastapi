1c9539b0df1594bc279e62732c019d3fd3aa6753
eddy.di <eddy.di.fint@gmail.com>
2024-01-21 18:04:56 +0600

    minor changes

--- models.py
+++ models.py
@@ -1,18 +1,14 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
-from database import Base
-from sqlalchemy.ext.declarative import declarative_base
+from database import Base, engine
+from sqlalchemy import Column, ForeignKey, String, Float
 from sqlalchemy.orm import relationship
 from uuid import uuid4
-from database import engine
-
-Base = declarative_base()
 
 class Menu(Base):
     __tablename__ = 'menus'
 
     id = Column(String, primary_key=True, default=str(uuid4()))
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String)
+    description = Column(String)
 
     submenus = relationship("SubMenu", back_populates="menu", cascade="all, delete-orphan")
 
@@ -21,12 +17,12 @@ class SubMenu(Base):
     __tablename__ = 'submenus'
 
     id = Column(String, primary_key=True, default=str(uuid4()))
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String)
+    description = Column(String)
 
     menu_id = Column(String, ForeignKey("menus.id", ondelete='CASCADE'))
-    
     menu = relationship('Menu', back_populates='submenus')
+
     dishes = relationship("Dish", back_populates="submenu", cascade="all, delete-orphan")
 
 
@@ -35,12 +31,11 @@ class Dish(Base):
     __tablename__ = 'dishes'
 
     id = Column(String, primary_key=True, default=str(uuid4()))
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    price = Column(Float, index=True)
+    title = Column(String)
+    description = Column(String)
+    price = Column(Float)
 
     submenu_id = Column(String, ForeignKey("submenus.id", ondelete='CASCADE'))
-    
     submenu = relationship('SubMenu', back_populates='dishes')
 
 
