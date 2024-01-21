e85d802da31c184e0c430a122ee8abd9323b4ee0
eddy.di <eddy.di.fint@gmail.com>
2024-01-21 22:02:40 +0600

    update safe database url path

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://menu_db_admin:menu_db_admin@localhost:5432/menu_db"
+SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
