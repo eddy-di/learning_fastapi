480ff830e476d93e490c17755f4b54388cf9c189
eddy.di <eddy.di.fint@gmail.com>
2024-01-23 10:57:51 +0600

    update readme, test env

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Load .env file
+load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
+# SQLALCHEMY_DATABASE_URL = "postgresql://menu_db_admin:menu_db_admin@localhost:5432/menu_db" # 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
