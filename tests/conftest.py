b5f4adf1da7f191ae7995ec897e99368d5467ea5
eddy.di <eddy.di.fint@gmail.com>
2024-02-12 00:12:41 +0600

    update case4 tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,8 @@ def event_loop(request) -> Generator:  # noqa: indirect usage
     yield loop
     loop.close()
 
+# unit tests
+
 
 @pytest_asyncio.fixture(scope='function')
 async def async_session() -> AsyncSession:
@@ -56,6 +58,50 @@ async def async_client(async_session):
             base_url='http://test'
     ) as client:
         yield client
+
+# scenario
+
+
+@pytest_asyncio.fixture(scope='module')
+async def scenario_session() -> AsyncSession:
+    session = sessionmaker(
+        async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with session() as s:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+        yield s
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+    await async_engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def scenario_client(scenario_session):
+    def override_get_db() -> Iterator[AsyncSession]:
+        """Utility function to wrap the database session in a generator.
+
+        Yields:
+            Iterator[AsyncSession]: An iterator containing one database session.
+        """
+        yield scenario_session
+
+    app.dependency_overrides[get_async_db] = override_get_db
+
+    async with AsyncClient(
+            app=app,
+            base_url='http://test'
+    ) as client:
+        yield client
+
+
+async def drop_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest_asyncio.fixture(scope='function')
--- tests/test_case4_counters.py
+++ tests/test_case4_counters.py
@@ -1,5 +1,10 @@
 import uuid
 
+import pytest
+
+# from .conftest import async_client
+from httpx import AsyncClient
+
 from app.routers.dish import create_dish, get_dishes
 from app.routers.menu import create_menu, delete_menu, get_menu, get_menus
 from app.routers.submenu import (
@@ -10,17 +15,14 @@ from app.routers.submenu import (
 )
 from app.utils.pathfinder import reverse
 
-from .conftest import client
-
 menu_id = None
 submenu_id = None
 dish1_id = None
 dish2_id = None
 
 
-def test_create_menu(init_db, cache_connect):
-    init_db
-    cache_connect
+@pytest.mark.asyncio
+async def test_create_menu(scenario_client: AsyncClient,):
     # given: empty db with initialized tables
     global menu_id
     url = reverse(create_menu)
@@ -29,7 +31,7 @@ def test_create_menu(init_db, cache_connect):
         'title': 'My menu 1',
         'description': 'My menu description 1'
     }
-    response = client.post(url, json=data)
+    response = await scenario_client.post(url, json=data)
     # then: expecting to get passed data in response, save generated id in global variable
     assert response.status_code == 201
     assert response.json()['title'] == 'My menu 1'
@@ -39,7 +41,8 @@ def test_create_menu(init_db, cache_connect):
     menu_id = response.json()['id']
 
 
-def test_submenu_create():
+@pytest.mark.asyncio
+async def test_submenu_create(scenario_client: AsyncClient,):
     # given: menu instance
     global menu_id
     global submenu_id
@@ -47,9 +50,10 @@ def test_submenu_create():
     # when: executing POST operation for submenu instance
     submenu_data = {
         'title': 'My submenu 1',
-        'description': 'My submenu description 1'
+        'description': 'My submenu description 1',
+        'menu_id': menu_id
     }
-    response = client.post(submenus_url, json=submenu_data)
+    response = await scenario_client.post(submenus_url, json=submenu_data)
     # then: expecting to get passed data in response, save generated id in global variable
     assert response.status_code == 201
     assert response.json()['title'] == 'My submenu 1'
@@ -59,7 +63,8 @@ def test_submenu_create():
     submenu_id = response.json()['id']
 
 
-def test_dish_create_case1():
+@pytest.mark.asyncio
+async def test_dish_create_case1(scenario_client: AsyncClient,):
     # given: menu and linked submenu instance
     global menu_id
     global submenu_id
@@ -71,7 +76,7 @@ def test_dish_create_case1():
         'description': 'My dish description 2',
         'price': '13.50'
     }
-    response = client.post(dish_url, json=dish1_data)
+    response = await scenario_client.post(dish_url, json=dish1_data)
     # then: expecting to get passed data in response, save generated id in global variable
     assert response.status_code == 201
     assert response.json()['title'] == 'My dish 2'
@@ -82,7 +87,8 @@ def test_dish_create_case1():
     dish1_id = response.json()['id']
 
 
-def test_dish_create_case2():
+@pytest.mark.asyncio
+async def test_dish_create_case2(scenario_client: AsyncClient,):
     # given: menu and linked submenu instance
     global menu_id
     global submenu_id
@@ -94,7 +100,7 @@ def test_dish_create_case2():
         'description': 'My dish description 1',
         'price': '12.50'
     }
-    response = client.post(dish_url, json=dish2_data)
+    response = await scenario_client.post(dish_url, json=dish2_data)
     # then: expecting to get passed data in response, save generated id in global variable
     assert response.status_code == 201
     assert response.json()['title'] == 'My dish 1'
@@ -105,12 +111,13 @@ def test_dish_create_case2():
     dish2_id = response.json()['id']
 
 
-def test_get_counters_for_target_menu():
+@pytest.mark.asyncio
+async def test_get_counters_for_target_menu(scenario_client: AsyncClient,):
     # given: menu linked with submenu and two dish instances
     global menu_id
     url = reverse(get_menu, target_menu_id=menu_id)
     # when: executing GET operation for target menu
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and submenus_count == 1, dishes_count == 2
     assert response.status_code == 200
     assert response.json()['submenus_count'] == 1
@@ -118,59 +125,64 @@ def test_get_counters_for_target_menu():
     assert response.json()['id'] == menu_id
 
 
-def test_get_counters_for_target_submenu():
+@pytest.mark.asyncio
+async def test_get_counters_for_target_submenu(scenario_client: AsyncClient,):
     # given: menu linked with submenu and two dish instances
     global menu_id
     global submenu_id
     url = reverse(get_submenu, target_menu_id=menu_id, target_submenu_id=submenu_id)
     # when: executing GET operation for target submenu
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and dishes_count == 2
     assert response.status_code == 200
     assert response.json()['dishes_count'] == 2
     assert response.json()['id'] == submenu_id
 
 
-def test_delete_target_submenu():
+@pytest.mark.asyncio
+async def test_delete_target_submenu(scenario_client: AsyncClient,):
     # given: menu linked with submenu and two dish instances
     global menu_id
     global submenu_id
     url = reverse(delete_submenu, target_menu_id=menu_id, target_submenu_id=submenu_id)
     # when: executing DELETE operation for target submenu
-    response = client.delete(url)
+    response = await scenario_client.delete(url)
     # then: expecting to get status code 200
     assert response.status_code == 200
 
 
-def test_get_submenu_list():
+@pytest.mark.asyncio
+async def test_get_submenu_list(scenario_client: AsyncClient,):
     # given: menu instance
     global menu_id
     url = reverse(get_submenus, target_menu_id=menu_id)
     # when: executing GET operation for submenu list
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and empty submenus
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_get_dishes_list():
+@pytest.mark.asyncio
+async def test_get_dishes_list(scenario_client: AsyncClient,):
     # given: menu instance
     global menu_id
     global submenu_id
     url = reverse(get_dishes, target_menu_id=menu_id, target_submenu_id=submenu_id)
     # when: executing GET operation for dishes list
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and empty dishes
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_get_target_menu():
+@pytest.mark.asyncio
+async def test_get_target_menu(scenario_client: AsyncClient,):
     # given: menu instance
     global menu_id
     url = reverse(get_menu, target_menu_id=menu_id)
     # when: executing GET operation for target menu
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and submenus_count == 0, dishes_count == 0
     assert response.status_code == 200
     assert response.json()['submenus_count'] == 0
@@ -178,27 +190,30 @@ def test_get_target_menu():
     assert response.json()['id'] == menu_id
 
 
-def test_delete_target_menu():
+@pytest.mark.asyncio
+async def test_delete_target_menu(scenario_client: AsyncClient,):
     # given: menu instance
     global menu_id
     url = reverse(delete_menu, target_menu_id=menu_id)
     # when: executing DELETE operation for target menu
-    response = client.delete(url)
+    response = await scenario_client.delete(url)
     # then: expecting to get status code 200
     assert response.status_code == 200
 
 
-def test_get_menus_list():
+@pytest.mark.asyncio
+async def test_get_menus_list(scenario_client: AsyncClient,):
     # given: empty db
     url = reverse(get_menus)
     # when: executing GET operation for menu list
-    response = client.get(url)
+    response = await scenario_client.get(url)
     # then: expecting to get status code 200 and empty menus
     assert response.status_code == 200
     assert response.json() == []
 
-
-def test_end(drop_db, cache_flush):
-    # necessary to cleanup db
-    drop_db
-    cache_flush
+# @pytest.mark.asyncio
+# async def test_end(
+    # cache_flush
+# ):
+    # ###necessary to cleanup db
+    # cache_flush
