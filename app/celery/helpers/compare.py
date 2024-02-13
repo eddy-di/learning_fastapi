6e14cf3ebff3104bc47cba8f8a0a8c9a17dda105
eddy.di <eddy.di.fint@gmail.com>
2024-02-14 02:08:08 +0600

    fix links in compare functions

--- app/celery/helpers/compare.py
+++ app/celery/helpers/compare.py
@@ -11,19 +11,6 @@ from app.config.base import (
 )
 
 
-def get_correct_link(model_name: str, menu_id: str, submenu_id: str | None = None, dish_id: str | None = None):
-    if model_name == 'menu':
-        return SERVER_URL + MENU_LINK.format(target_menu_id=menu_id)
-
-    if model_name == 'submenu':
-        return SERVER_URL + SUBMENU_LINK.format(
-            target_menu_id=menu_id,
-            target_submenu_id=submenu_id,
-        )
-
-    return SERVER_URL + DISH_LINK.format(target_menu_id=menu_id, target_submenu_id=submenu_id, target_dish_id=dish_id,)
-
-
 def compare_models_data(excel_data: dict, db_data: dict):
     excel_keys = excel_data.keys()
     db_keys = db_data.keys()
@@ -37,45 +24,37 @@ def compare_models_data(excel_data: dict, db_data: dict):
 def crud_menus(excel_data: dict, db_data: dict):
     ids_to_create, ids_to_update, ids_to_delete = compare_models_data(excel_data, db_data)
 
-    for id in ids_to_create:
+    for menu_id in ids_to_create:
         url = SERVER_URL + MENUS_LINK
-        requests.post(url, json=excel_data[id])
+        requests.post(url, json=excel_data[menu_id])
 
-    for id in ids_to_update:
-        url = get_correct_link(
-            model_name='menu',
-            menu_id=id,
-        )
-        requests.patch(url, json=excel_data[id])
+    for menu_id in ids_to_update:
+        url = SERVER_URL + MENU_LINK.format(target_menu_id=menu_id)
+        requests.patch(url, json=excel_data[menu_id])
 
-    for id in ids_to_delete:
-        url = get_correct_link(
-            model_name='menu',
-            menu_id=id,
-        )
+    for menu_id in ids_to_delete:
+        url = SERVER_URL + MENU_LINK.format(target_menu_id=menu_id)
         requests.delete(url)
 
 
 def crud_submenus(excel_data: dict, db_data: dict):
     ids_to_create, ids_to_update, ids_to_delete = compare_models_data(excel_data, db_data)
 
-    for id in ids_to_create:
-        url = SERVER_URL + SUBMENUS_LINK.format(target_menu_id=excel_data[id]['menu_id'])
-        requests.post(url, json=excel_data[id])
+    for submenu_id in ids_to_create:
+        url = SERVER_URL + SUBMENUS_LINK.format(target_menu_id=excel_data[submenu_id]['menu_id'])
+        requests.post(url, json=excel_data[submenu_id])
 
-    for id in ids_to_update:
-        url = get_correct_link(
-            model_name='submenu',
-            menu_id=excel_data[id]['menu_id'],
-            submenu_id=id
+    for submenu_id in ids_to_update:
+        url = SERVER_URL + SUBMENU_LINK.format(
+            target_menu_id=excel_data[submenu_id]['menu_id'],
+            target_submenu_id=submenu_id,
         )
-        requests.patch(url, json=excel_data[id])
+        requests.patch(url, json=excel_data[submenu_id])
 
-    for id in ids_to_delete:
-        url = get_correct_link(
-            model_name='submenu',
-            menu_id=excel_data[id]['menu_id'],
-            submenu_id=id
+    for submenu_id in ids_to_delete:
+        url = SERVER_URL + SUBMENU_LINK.format(
+            target_menu_id=excel_data[submenu_id]['menu_id'],
+            target_submenu_id=submenu_id,
         )
         requests.delete(url)
 
@@ -83,27 +62,25 @@ def crud_submenus(excel_data: dict, db_data: dict):
 def crud_dishes(excel_data: dict, db_data: dict):
     ids_to_create, ids_to_update, ids_to_delete = compare_models_data(excel_data, db_data)
 
-    for id in ids_to_create:
+    for dish_id in ids_to_create:
         url = SERVER_URL + DISHES_LINK.format(
-            target_menu_id=excel_data[id]['menu_id'],
-            target_submenu_id=excel_data[id]['submenu_id']
+            target_menu_id=excel_data[dish_id]['menu_id'],
+            target_submenu_id=excel_data[dish_id]['submenu_id']
         )
-        requests.post(url, json=excel_data[id])
+        requests.post(url, json=excel_data[dish_id])
 
-    for id in ids_to_update:
-        url = get_correct_link(
-            model_name='dish',
-            menu_id=excel_data[id]['menu_id'],
-            submenu_id=excel_data[id]['submenu_id'],
-            dish_id=id
+    for dish_id in ids_to_update:
+        url = SERVER_URL + DISH_LINK.format(
+            target_menu_id=excel_data[dish_id]['menu_id'],
+            target_submenu_id=excel_data[dish_id]['submenu_id'],
+            dish_id=dish_id
         )
-        requests.patch(url, json=excel_data[id])
+        requests.patch(url, json=excel_data[dish_id])
 
-    for id in ids_to_delete:
-        url = get_correct_link(
-            model_name='dish',
-            menu_id=excel_data[id]['menu_id'],
-            submenu_id=excel_data[id]['submenu_id'],
-            dish_id=id
+    for dish_id in ids_to_delete:
+        url = SERVER_URL + DISH_LINK.format(
+            target_menu_id=excel_data[dish_id]['menu_id'],
+            target_submenu_id=excel_data[dish_id]['submenu_id'],
+            dish_id=dish_id
         )
         requests.delete(url)
